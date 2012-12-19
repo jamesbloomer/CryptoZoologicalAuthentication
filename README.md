3ae671942c612c37a6e7b60ce1352c8a5668b052
James Bloomer <github2@thebloomers.co.uk>
2012-12-19 01:50:33 -0500

    add tests and edit packagae.json

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 CryptoZoologicalAuthentication
 ==============================
+
+Use animals for passwords when you really can't be bothered to do proper authentication.
+
+## usage
+
+``` javascript
+var cza = require('cryptozoologicalauthentication');
+if (cza("ike", "iguanakoalaelephant") === true ) {
+    // authenticated!
+}
+```
--- test/index_test.js
+++ test/index_test.js
@@ -1,10 +1,21 @@
 var assert = require('assert'),
+    mocha = require('mocha'),
     cza = require('..\/');
 
-var invalid_password_should_return_false = function() {
-    assert.equal(cza("Ike", "wrong"), false);
-};
-
-var correct_password_should_return_true = function() {
-    assert.equal(cza("Ike", "iguanakoalaelephant"), true);
-};
+describe('CryptoZoologicalAuthentication', function(){
+    it('invalid password should return false', function(){
+        assert.equal(cza("ike", "wrong"), false);
+        assert.equal(cza("dave", "password"), false);
+        assert.equal(cza("sarah", "1234"), false);
+        assert.equal(cza("dave", "secret"), false);
+    });
+    
+    it('correct password should return true', function(){
+        assert.equal(cza("ike", "iguanakoalaelephant"), true);
+        assert.equal(cza("abc", "antbatcat"), true);
+        assert.equal(cza("def", "dogelephantfrog"), true);
+        assert.equal(cza("keith", "koalaelephantiguana"), true);
+        assert.equal(cza("dave", "dogantvole"), true);
+        assert.equal(cza("sarah", "snakeantrat"), true);
+    });
+});
